a2b05c205bdca8d24882d1ded0b189db19bd3885
sudipanag <49046853+sudipanag@users.noreply.github.com>
2020-07-31 19:56:11 -0400

    Add files via upload

---
+++ JWTAuthentication_TokenBarer/Services/IAuthenticateService.cs
@@ -0,0 +1,14 @@
+﻿using JWTAuthentication_TokenBarer.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace JWTAuthentication_TokenBarer.Services
+{
+    public interface IAuthenticateService
+    {
+        Student Authenticate(String UserName, String Password);
+    }
+}
+
