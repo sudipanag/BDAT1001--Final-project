c85a7c3ab2f084079eb819b967e0b2c233069f48
sudipanag <49046853+sudipanag@users.noreply.github.com>
2020-07-31 20:01:09 -0400

    Add files via upload

---
+++ JWTAuthentication_TokenBarer/SalesData.cs
@@ -0,0 +1,15 @@
+using System;
+
+namespace JWTAuthentication_TokenBarer
+{
+    public class SalesData
+    {
+        public int OrderId { get; set; }
+
+        public string Productname { get; set; }
+
+        public int Amount { get; set; }
+
+        public int Quantity { get; set; }
+    }
+}
